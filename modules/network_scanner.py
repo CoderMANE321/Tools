dce15d7f9f5bf13623c9de0a351f7a178c83c54a
CoderMANE321 <deontamwilliams@gmail.com>
2025-02-14 16:17:55 -0500

    [network scan grab ip automatically]

--- modules/network_scanner.py
+++ modules/network_scanner.py
@@ -1,46 +1,34 @@
 #!/usr/bin/env python
 """
 Program Name: network scanner
-Description: Scans the network and provides the ip and mac address of the network users
-
-
-Options:
-    -i , or --i = ip address
-    --help = argument options
-
-
-Examples:
-    python network_scanner.py -i or --i
-    python network_scanner.py --help
-    python network_scanner -i 192.22.33.123
+Description: Scans the network and provides the IP and MAC address of network users.
 
 Requirements:
     Runs on Python 2 or 3
 
 Author: CoderMANE
-
 Date: 2024-06-09
 """
-import argparse
 
 import scapy.all as scapy
+import socket
 
-def get_arguments():
-    # parse user inputs
-    parser = argparse.ArgumentParser()
-    # describes options
-    parser.add_argument("-i", "--ip", dest="ip", help="Ip to scan")
-    # return arguments
-    options = parser.parse_args()
-    if not options.ip:
-        parser.error("[-] Please specify an ip address, use --help for more info.")
-    return options
-# scans and returns clients into a dictonary
+def get_local_ip():
+    """Get the local IP address of the current machine."""
+    try:
+        hostname = socket.gethostname()
+        local_ip = socket.gethostbyname(hostname)
+        return local_ip
+    except Exception as e:
+        print(f"Error getting local IP: {e}")
+        return None
+
+# Scans and returns clients into a dictionary
 def scan(ip):
-    arp_request = scapy.ARP(pdst=ip)
+    arp_request = scapy.ARP(pdst=ip + "/24")  # Scan the entire subnet
     broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
-    arp_request_broadcast = broadcast/arp_request
-    answered_list, unanswered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
+    arp_request_broadcast = broadcast / arp_request
+    answered_list, _ = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)
 
     clients_list = []
     for element in answered_list:
@@ -48,14 +36,16 @@ def scan(ip):
         clients_list.append(client_dict)
     return clients_list
 
-# handles output
+# Handles output
 def print_result(results_list):
     print("IP\t\t\tMAC Address\n----------------------------------------------")
     for client in results_list:
         print(client["ip"] + "\t\t" + client["mac"])
 
-
-# call and use function
-options = get_arguments()
-scan_result = scan(options.ip)
-print_result(scan_result)
+# Get local IP and scan the network
+local_ip = get_local_ip()
+if local_ip:
+    scan_result = scan(local_ip)
+    print_result(scan_result)
+else:
+    print("Could not determine local IP.")
