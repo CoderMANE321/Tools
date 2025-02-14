e5d40ff22a9063996e10a1a5c0f885522e603788
CoderMANE321 <deontamwilliams@gmail.com>
2025-02-14 16:22:05 -0500

    [mac changer CLI]

--- modules/mac_changer.py
+++ modules/mac_changer.py
@@ -1,74 +1,63 @@
 #!/usr/bin/env python
 """
-Program Name: mac changer
+Program Name: MAC Changer
 
-Description: Changes the mac address of given machine ip
-
-Options:
-    -i or --i = ip address
-    -m or --mac = mac address
-    --help = argument options
-
-
-Examples:
-    python mac_changer.py -i or --i with -m or --mac
-    python mac_changer.py --help
+Description: Changes the MAC address of the machine's network interface.
 
 Requirements:
     Runs on Python 2 or 3
 
 Author: CoderMANE
-
 Date: 2024-06-09
 """
+
 import subprocess
-import optparse
 import re
 
-# collects arguments
-def get_arguments():
-    # parse user inputs
-    parser = optparse.OptionParser()
-    # describes options
-    parser.add_option("-i", "--interface", dest="interface", help="Interface to change its MAC address")
-    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-    (options, arguments) = parser.parse_args()
-    if not options.interface:
-        parser.error("[-] Please specify an interface, use --help for more info.")
-    elif not options.new_mac:
-        parser.error("[-] Please specify an new mac, use --help for more info.")
-    return options
+# Automatically detects the primary network interface
+def get_interface():
+    try:
+        ifconfig_result = subprocess.check_output(["ip", "link"], text=True)
+        interfaces = re.findall(r"^\d+: (\w+):", ifconfig_result, re.MULTILINE)
+        for iface in interfaces:
+            if iface != "lo":  # Ignore the loopback interface
+                return iface
+    except Exception as e:
+        print(f"Error detecting interface: {e}")
+    return None
 
-# changes mac address
+# Changes MAC address
 def change_mac(interface, new_mac):
-    print("[+ Changing MAC address for " + interface + " to " + new_mac)
+    print(f"[+] Changing MAC address for {interface} to {new_mac}")
+    subprocess.call(["sudo", "ip", "link", "set", interface, "down"])
+    subprocess.call(["sudo", "ip", "link", "set", interface, "address", new_mac])
+    subprocess.call(["sudo", "ip", "link", "set", interface, "up"])
 
-    subprocess.call(["ifconfig", interface, "down"])
-    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-    subprocess.call(["ifconfig", interface, "up"])
-
-# collects the current address for check later in program
+# Gets current MAC address
 def get_current_mac(interface):
-    ifconfig_result = subprocess.check_output(["ifconfig", options.interface])
-    mac_address_search_result = re.search(r"\w\w:\w\w:\w\w:\w\w:\w\w:\w\w", str(ifconfig_result))
+    try:
+        ifconfig_result = subprocess.check_output(["ip", "link", "show", interface], text=True)
+        mac_address_search_result = re.search(r"(\w\w:\w\w:\w\w:\w\w:\w\w:\w\w)", ifconfig_result)
+        if mac_address_search_result:
+            return mac_address_search_result.group(0)
+    except Exception as e:
+        print(f"Error getting MAC address: {e}")
+    return None
 
-    if mac_address_search_result:
-        return mac_address_search_result.group(0)
-    else:
-        print("[-] Could not read MAC address")
-
-
-# call and use functions
-options = get_arguments()
-
-current_mac = get_current_mac(options.interface)
-print("Current MAC = " + str(current_mac))
-
-change_mac(options.interface, options.new_mac)
-
-current_mac = get_current_mac(options.interface)
-# checks if program was sucessful
-if current_mac == options.new_mac:
-    print("[+] MAC address was successfully changed to " + current_mac)
+# Main execution
+interface = get_interface()
+if not interface:
+    print("[-] No valid network interface found.")
 else:
-    print("[-] MAC address didn't change")
+    print(f"Detected interface: {interface}")
+    current_mac = get_current_mac(interface)
+    print(f"Current MAC: {current_mac}")
+
+    new_mac = "00:11:22:33:44:55"  # Change this to your desired MAC address
+    change_mac(interface, new_mac)
+
+    current_mac = get_current_mac(interface)
+    if current_mac == new_mac:
+        print(f"[+] MAC address successfully changed to {current_mac}")
+    else:
+        print("[-] MAC address did not change.")
